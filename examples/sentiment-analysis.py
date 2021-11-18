8697aba4c69358a0cbf4a39795215d908c1ffaa6
mbannert <bunny@hey.com>
2021-11-18 12:11:37 +0100

    added basic google nlp sentiment analysis example

---
+++ examples/sentiment-analysis.py
@@ -0,0 +1,50 @@
+"""Demonstrates how to make a simple call to the Natural Language API."""
+
+import argparse
+
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+
+def print_result(annotations):
+    score = annotations.document_sentiment.score
+    magnitude = annotations.document_sentiment.magnitude
+
+    for index, sentence in enumerate(annotations.sentences):
+        sentence_sentiment = sentence.sentiment.score
+        print('Sentence {} has a sentiment score of {}'.format(
+            index, sentence_sentiment))
+
+    print('Overall Sentiment: score of {} with magnitude of {}'.format(
+        score, magnitude))
+    return 0
+
+
+def analyze(movie_review_filename):
+    """Run a sentiment analysis request on text within a passed filename."""
+    client = language.LanguageServiceClient()
+
+    with open(movie_review_filename, 'r') as review_file:
+        # Instantiates a plain text document.
+        content = review_file.read()
+
+    document = types.Document(
+        content=content,
+        type=enums.Document.Type.PLAIN_TEXT)
+    annotations = client.analyze_sentiment(document=document)
+
+    # Print the results
+    print_result(annotations)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        'movie_review_filename',
+        help='The filename of the movie review you\'d like to analyze.')
+    args = parser.parse_args()
+
+    analyze(args.movie_review_filename)
